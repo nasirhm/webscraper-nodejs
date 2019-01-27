76478a7400f8ca756b48e83d9d05152525e444d0
Nasir Hussain <nasirhussainm14@gmail.com>
2019-01-28 04:47:18 +0500

    Complete Pages Parser

---
+++ parsePresidentToBDate.js
@@ -0,0 +1,17 @@
+const rp = require('request-promise');
+const cheer = require('cheerio');
+
+const parserUrl = function(url){
+return rp(url)
+    .then(function(data){
+        return {
+        name : cheer('.firstheading',data).text(),
+        Birthday : cheer('.bday',data).text()
+    };
+    })
+    .catch(function(err){
+        console.log(err);
+    })
+}
+
+module.exports = parserUrl;
