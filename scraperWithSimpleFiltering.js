e4f0d6cbef880b033ef629e73f4a24fe5e62ad81
Nasir Hussain <nasirhussainm14@gmail.com>
2019-01-28 04:23:19 +0500

    Easy Filter Cheer

--- scraperWithSimpleFiltering.js
+++ scraperWithSimpleFiltering.js
@@ -1,12 +1,13 @@
 const rp = require('request-promise');
 const cheer = require('cheerio');
-const url = 'https://www.indeed.com/q-Blockchain-jobs.html';
+const url = 'https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States';
 
 
 rp(url)
     .then(function(data){
-        console.log(cheer('div > h2',data.length));
-        console.log(cheer('div > h2',data))
+        //Filtering Tags - Length one is to check there isn't additional Data Coming
+        console.log(cheer('big > a',data.length));
+        console.log(cheer('big > a',data))
     })
     .catch(function(err){
         console.log(err);
