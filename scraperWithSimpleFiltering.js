850f8cc629b26cfc152eb10bb97887e0402373cf
Nasir Hussain <nasirhussainm14@gmail.com>
2019-01-27 20:22:49 +0500

    variable mistake fixed

--- scraperWithSimpleFiltering.js
+++ scraperWithSimpleFiltering.js
@@ -5,8 +5,8 @@ const url = 'https://en.wikipedia.org/wiki/List_of_Presidents_of_Pakistan';
 
 rp(url)
     .then(function(data){
-        console.log(cheeer('tr > th > a',html.length));
-        console.log(cheer('tr > tr > a',html))
+        console.log(cheer('tr > th > a',data.length));
+        console.log(cheer('tr > th > a',data))
     })
     .catch(function(err){
         console.log(err);
