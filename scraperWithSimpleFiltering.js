0467acbb64f7f8c56f0b796bc4580be45e1f3fc5
Nasir Hussain <nasirhussainm14@gmail.com>
2019-01-27 20:21:43 +0500

    code

--- scraperWithSimpleFiltering.js
+++ scraperWithSimpleFiltering.js
@@ -1,9 +1,12 @@
 const rp = require('request-promise');
+const cheer = require('cheerio');
 const url = 'https://en.wikipedia.org/wiki/List_of_Presidents_of_Pakistan';
+
 
 rp(url)
     .then(function(data){
-        console.log(data);
+        console.log(cheeer('tr > th > a',html.length));
+        console.log(cheer('tr > tr > a',html))
     })
     .catch(function(err){
         console.log(err);
