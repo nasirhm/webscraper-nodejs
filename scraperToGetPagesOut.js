64d508c0d40f9d123ecd277307635b14b349f7e1
Nasir Hussain <nasirhussainm14@gmail.com>
2019-01-28 04:35:31 +0500

    done fetching urls

--- scraperToGetPagesOut.js
+++ scraperToGetPagesOut.js
@@ -4,9 +4,19 @@ const url = 'https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_Stat
 
 rp(url)
     .then(function(html){
-
+        //making array
+        const urls = [];
+        
+        //for loop
+        for(i = 0; i<45; i++){
+            //Pushing the Scraped Data in an Array
+            urls.push(cheer('big > a',html)[i].attribs.href);
+        }
+        console.log(urls);
     }
     )
-    .error(funtion(err){
-        console.log(err);
+    .catch(function(err)
+    {
+        //error handling
+        console.log(err)
     })
