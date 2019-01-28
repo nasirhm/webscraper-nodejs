77763f116d2ddc30cd2e70b3cdd9d2605c546c7e
Nasir Hussain <nasirhussainm14@gmail.com>
2019-01-28 05:28:11 +0500

    Puppeteer DOne

--- puppeteerScraperReddit.js
+++ puppeteerScraperReddit.js
@@ -2,3 +2,20 @@ const puppeteer = require('puppeteer');
 //Puppeteer is a Headless Browser by Google for Developers
 
 const url = 'https://www.reddit.com';
+
+puppeteer
+    .launch()
+    .then(function(browser){
+        return browser.newPage();
+    })
+    .then(function(page){
+        return page.goto(url).then(function(){
+            return page.content();
+        });
+    })
+    .then(function(html){
+        console.log(html);
+    })
+    .catch(function(err){
+        console.log(err);
+    });
