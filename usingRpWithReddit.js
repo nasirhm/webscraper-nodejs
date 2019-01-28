c51685645d39b0933c923a964ddee41a87749fcc
Nasir Hussain <nasirhussainm14@gmail.com>
2019-01-28 05:14:57 +0500

    Done

--- usingRpWithReddit.js
+++ usingRpWithReddit.js
@@ -1,2 +1,14 @@
 // Here to fetch JS data we're using Puppeteer
 
+const rp = require('request-promise');
+const url = 'https://www.reddit.com';
+
+rp(url)
+  .then(function(html){
+    //success!
+    console.log(html);
+  })
+  .catch(function(err){
+    //handle error
+  });
+}
